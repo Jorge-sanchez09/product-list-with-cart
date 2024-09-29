209e7263582a2123958236cb2fda6def7e7653a6
Jorge Sanchez <jorge.humberto.sq@gmail.com>
2024-09-29 16:33:19 -0700

    base

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/product-list-with-cart/",
   css: {
     devSourcemap: true,
   },
-  base: "/product-list-with-cart/",
 });
